8ea0a1f72ef78dc3c669995b887791acd3013cc7
Dominic Kempf <dominic.r.kempf@gmail.com>
2014-11-26 14:34:35 +0100

    Allow the ini file to have assignment operators with more than one character
    This is essential to an upcoming project

--- python/parseIni.py
+++ python/parseIni.py
@@ -6,7 +6,7 @@ TODO:
 
 # inspired by http://www.decalage.info/fr/python/configparser
 
-def parse_ini_file(filename, commentChar = ("#",), assignChar=("=",":"), asStrings=True, conversionList=(int, float,)):
+def parse_ini_file(filename, commentChar=("#",), assignment="=", asStrings=False, conversionList=(int, float,)):
     """ parse Dune style .ini files into a dictionary
        
     The parser behaviour can be customized by the keyword arguments of this function.
@@ -18,13 +18,12 @@ def parse_ini_file(filename, commentChar = ("#",), assignChar=("=",":"), asStrin
     commentChar: list
         A list of characters that define comments. Everything on a line
         after such character is ignored during the parsing process.
-    assignChar : list
-        A list of characters that separates the key from the value on a line 
+    assignment : string
+        A string that separates the key from the value on a line
         containing a key/value pair.
     asStrings : bool
-        Whether the values should be treated as strings. At the moment, this
-        defaults to true (until better stuff is implemented)
-    conversionList : list 
+        Whether the values should be treated as strings.
+    conversionList : list
         A list of functions to try for converting the parsed strings to other types
         The order of the functions defines the priority of the conversion rules
         (highest priority first). All conversion rules are expected to raise
@@ -40,10 +39,10 @@ def parse_ini_file(filename, commentChar = ("#",), assignChar=("=",":"), asStrin
         # strip comments from the line
         for char in commentChar:
             if char in line:
-                line, comment = line.split(char, 1)        
-            
+                line, comment = line.split(char, 1)
+
         # check whether this line specifies a group
-        if "[" in line and "]" in line:
+        if ("[" in line) and ("]" in line):
             # reset the current dictionary
             current_dict = result_dict
        
@@ -68,34 +67,37 @@ def parse_ini_file(filename, commentChar = ("#",), assignChar=("=",":"), asStrin
         # this is necessary to have some subgroup definitions in keys instead of in square brackets.
         group_dict = current_dict
 
-        # check whether this line defines a key
-        for char in assignChar:
-            if char in line:
-                # split key from value
-                key, value = line.split(char, 1)
-                
-                # look for additional groups in the key
-                key = key.strip()
-                while "." in key:
-                    group, key = key.split(".")
-                    if group not in current_dict:
-                        current_dict[group] = {}
-                    current_dict = current_dict[group]
+        # check whether this line defines a key/value pair
+        # only process if the assignment string is found exactly once
+        # 0 => no relevant assignment 2=> this is actually an assignment with a more complicated operator
+        if line.count(assignment) is 1:
+            # split key from value
+            import re
+            print re.split(char, line)
+            key, value = re.split(assignment, line)
 
-                # strip blanks from the value 
-                value = value.strip()
-                 
-                # set the dictionary entry for this pair to the default string
-                current_dict[key] = value
-                     
-                # check whether a given conversion applies to this key
-                if not asStrings:
-                    # iterate over the list of conversion functions in reverse priority order
-                    for rule in [x for x in reversed(conversionList)]:
-                        try:
-                            current_dict[key] = rule(value)
-                        except ValueError:
-                            pass
+            # look for additional groups in the key
+            key = key.strip()
+            while "." in key:
+                group, key = key.split(".")
+                if group not in current_dict:
+                    current_dict[group] = {}
+                current_dict = current_dict[group]
+
+            # strip blanks from the value
+            value = value.strip()
+
+            # set the dictionary entry for this pair to the default string
+            current_dict[key] = value
+
+            # check whether a given conversion applies to this key
+            if not asStrings:
+                # iterate over the list of conversion functions in reverse priority order
+                for rule in [x for x in reversed(conversionList)]:
+                    try:
+                        current_dict[key] = rule(value)
+                    except ValueError:
+                        pass
 
         # restore the current dictionary to the current group
         current_dict = group_dict
