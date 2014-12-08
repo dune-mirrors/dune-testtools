8c2d8edb221f4b68e29b924f530827ffdae5d92d
Dominic Kempf <dominic.r.kempf@gmail.com>
2014-12-08 10:56:35 +0100

    Add a specialization for the equidistant yaspgrid with non-zero origin

--- dune/testtools/gridconstruction.hh
+++ dune/testtools/gridconstruction.hh
@@ -89,11 +89,107 @@ public:
     grid->globalRefine(refinement);
   }
 
-  ~IniGridFactory() {
+  ~IniGridFactory()
+  {
     delete grid;
   }
 
-  Grid& getGrid() {
+  Grid& getGrid()
+  {
+    return *grid;
+  }
+
+private:
+  Grid* grid;
+};
+
+/** An IniGridFactory for equidistant YaspGrid with non-zero offset
+ *
+ * All keys are expected to be in group yaspgrid.
+ * The following keys are recognized:
+ * - lowerleft/origin : The coordinate of the lower left corner
+ * - upperright : The coordinate of the upper right corner.
+ * - extension : extension of the domain, only taken into account if no
+ *     upperright key was found.
+ * - cells : the number of cells in each direction
+ * - periodic : true or false for each direction
+ * - overlap : overlap size in cells
+ * - partitioning : a non standard load-balancing, number of processors per direction
+ * - keepPyhsicalOverlap : whether to keep the physical overlap
+ *     in physical size or in number of cells upon refinement
+ * - refinement : the number of global refines to apply initially.
+ */
+template<class ct, int dim>
+class IniGridFactory<
+    Dune::YaspGrid<dim, Dune::EquidistantOffsetCoordinates<ct, dim> > >
+{
+public:
+  typedef typename Dune::YaspGrid<dim,
+      Dune::EquidistantOffsetCoordinates<ct, dim> > Grid;
+
+  IniGridFactory(const Dune::ParameterTree& params)
+  {
+    // extract all constructor parameters from the ini file
+    // upper right corner
+    Dune::FieldVector<ct, dim> lowerleft =
+        params.get<Dune::FieldVector<ct, dim> >("yaspgrid.lowerleft");
+
+    Dune::FieldVector<ct, dim> upperright(lowerleft);
+    if (params.hasKey("upperright"))
+      upperright = params.get<Dune::FieldVector<ct, dim> >(
+          "yaspgrid.upperright");
+    else
+    {
+      Dune::FieldVector<ct, dim> extension = params.get<
+          Dune::FieldVector<ct, dim> >("yaspgrid.extension");
+      upperright += extension;
+    }
+
+    // number of cells per direction
+    std::array<int, dim> cells = params.get<std::array<int, dim> >(
+        "yaspgrid.cells");
+
+    // periodicity
+    std::bitset<dim> periodic;
+    periodic = params.get<std::bitset<dim> >("yaspgrid.periodic", periodic);
+
+    // overlap cells
+    int overlap = params.get<int>("yaspgrid.overlap", 1);
+
+    // (eventually) a non-standard load balancing
+    bool default_lb = true;
+    std::array<int, dim> partitioning;
+    if (params.hasKey("yaspgrid.partitioning"))
+    {
+      default_lb = false;
+      partitioning = params.get<std::array<int, dim> >("yaspgrid.partitioning");
+    }
+
+    // build the actual grid
+    if (default_lb)
+      grid = new Grid(lowerleft, upperright, cells, periodic, overlap);
+    else
+    {
+      typename Dune::YLoadBalanceBackup<dim> lb(partitioning);
+      grid = new Grid(lowerleft, upperright, cells, periodic, overlap,
+          typename Grid::CollectiveCommunicationType(), &lb);
+    }
+
+    bool keepPhysicalOverlap = params.get<bool>("yaspgrid.keepPhysicalOverlap",
+        true);
+    grid->refineOptions(keepPhysicalOverlap);
+
+    int refinement = params.get<int>("yaspgrid.refinement", 0);
+    grid->globalRefine(refinement);
+  }
+
+  ~IniGridFactory()
+  {
+    delete grid;
+  }
+
+  Grid& getGrid()
+  {
     return *grid;
   }
 
