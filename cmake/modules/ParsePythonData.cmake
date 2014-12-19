8f84b331f6000e280298f00a3b3cc124e9babd85
Dominic Kempf <dominic.r.kempf@gmail.com>
2014-12-19 14:47:21 +0100

    Use named arguments for the macro parse_python_data

--- cmake/modules/ParsePythonData.cmake
+++ cmake/modules/ParsePythonData.cmake
@@ -1,20 +1,25 @@
 # A CMake module defining macros to parse data from python scripts
 #
-# parse_python_data(prefix input)
+# parse_python_data(PREFIX prefix
+#                   INPUT input
+#                  )
 #
 # The <prefix> will be used for the variables. For each <key> in the data,
 # a variable <prefix>_<key> will be available in the scope of the call.
 # input: The string coming from the python output. Note, that it has to be
 # put in double quotes.
 
-function(parse_python_data prefix input)
+function(parse_python_data)
+  set(SINGLE PREFIX)
+  set(MULTI INPUT)
+  cmake_parse_arguments(PYPARSE "" "${SINGLE}" "${MULTI}" ${ARGN})
   # these keys are an agreement between the python and the cmake module
   # they can be changed to whatever keys, as long as they are updated on
   # both ends.
   set(SINGLEKEY __SEMICOLON)
   set(MULTIKEYS __SINGLE __MULTI __DATA)
   # first parsing: What keys are present in the data
-  cmake_parse_arguments(KEYS "" "${SINGLEKEY}" "${MULTIKEYS}" ${input})
+  cmake_parse_arguments(KEYS "" "${SINGLEKEY}" "${MULTIKEYS}" ${PYPARSE_INPUT})
 
   # second parsing: What data is associated with the keys
   cmake_parse_arguments(DATA "" "${KEYS___SINGLE}" "${KEYS___MULTI}" ${KEYS___DATA})
