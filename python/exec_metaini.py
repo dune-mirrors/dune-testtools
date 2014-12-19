e9ff17457c538985d1d2c1cb0a6ed8724bd10018
Dominic Kempf <dominic.r.kempf@gmail.com>
2014-12-19 17:36:33 +0100

    Extract the static section from a meta ini file and pass it to CMake
    With the filtering functionality of the meta ini parser,
    this does not need much code!

--- python/exec_metaini.py
+++ python/exec_metaini.py
@@ -1,6 +1,7 @@
 from compile_definitions import extract_compile_definitions
 from metaIni import expand_meta_ini
 from writeIni import write_dict_to_ini
+from cmakeoutput import printForCMake
 import sys
 
 if (len(sys.argv) is 2):
@@ -9,42 +10,24 @@ if (len(sys.argv) is 2):
 
     # here, we can modify the configurations, look for special keys,
     # extract information and do all sorts of weird stuff.
+    static_section = expand_meta_ini(sys.argv[1], filterKeys=["__STATIC", "__exec_suffix"], addNameKey=False)
 
-    # extract compile definitions from all the ini files that we got
-    static_confs, indices = extract_compile_definitions(configurations)
+    # construct a dictionary from the static information. This can be passed to CMake
+    # The special key __CONFIGS holds a list of configuration names
+    static = {}
+    static["__CONFIGS"] = []
+    for conf in static_section:
+        static["__CONFIGS"].append(conf["__exec_suffix"])
+        static[conf["__exec_suffix"]] = conf["__STATIC"]
 
-    # map compile definitions to executable suffixes
-    exec_suffix = {}
-    exec_suffix[""] = ""
-    for i, c in enumerate(configurations):
-        if "__exec_suffix" in c:
-            if (static_confs[indices[i]] in exec_suffix) and (exec_suffix[static_confs[indices[i]]] != c["__exec_suffix"]):
-                raise Error("Found two static configurations with different suffixes")
-            else:
-                exec_suffix[static_confs[indices[i]]] = c["__exec_suffix"]
-        else:
-            if len(static_confs) > 1:
-                raise KeyError("At the moment, all inis are expected to have an __exec_suffix key (if they have static information)")
-
-    # cook up something to return to cmake
-    returndef = ""
-    for sc in static_confs:
-        if returndef != "":
-            returndef = returndef + "\n"
-        # TODO make this nicer!
-        # using '&' as a separator is hopefully helpful with cmake list hell!
-        returndef = returndef + exec_suffix[sc] + "&" + sc
+    # print to CMake
+    printForCMake(static)
 
     # write the configurations to the file specified in the name key.
     for c in configurations:
         fn = c["__name"]
         del c["__name"]
         write_dict_to_ini(c, fn)
-
-    # This print statement is for cmake to read the standard output of this script.
-    # Ideally, this feature is enabled through an optional parameter to this script (TODO)
-    print returndef
-
 
 else:
     print "exec_metaIni expects exactly one command line parameter: the meta ini file"
