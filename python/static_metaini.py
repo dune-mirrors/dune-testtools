a7ccd88d09a63c6c9859c311c6f81001a1b39ba6
Timo Koch <koch_timo@hotmail.com>
2015-02-10 14:37:10 +0100

    Add error handling and default behaviour in static_metaini
    When calling the python script without having a __STATIC section in the ini file, may happen by accident
    the programme exited on a KeyError. Now an error message is thrown to std::err and the script exits (does not output anything to cmake).
    When not providing the special keyword __exec_suffix the programme exited on a KeyError. Now it appends an incrementing integer to the target name.

--- python/static_metaini.py
+++ python/static_metaini.py
@@ -1,5 +1,6 @@
 from metaIni import expand_meta_ini
 from cmakeoutput import printForCMake
+import sys
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -11,9 +12,14 @@ static_section = expand_meta_ini(args["ini"], filterKeys=["__STATIC", "__exec_su
 # determine a list of subgroups within the static section
 static_groups = []
 for conf in static_section:
-    for key in conf["__STATIC"]:
-        if (type(conf["__STATIC"][key]) is dict) and (key not in static_groups):
-            static_groups.append(key)
+    # check for __STATIC section. Who knows who may call this without having the section in the metaini-file
+    if "__STATIC" in conf:
+        for key in conf["__STATIC"]:
+            if (type(conf["__STATIC"][key]) is dict) and (key not in static_groups):
+                static_groups.append(key)
+    else:
+        sys.stderr.write("\n -- Static variant generation failed because no __STATIC section could be found in the meta-inifile!\n")
+        exit(0)
 
 # construct a dictionary from the static information. This can be passed to CMake
 # The special key __CONFIGS holds a list of configuration names
@@ -23,18 +29,26 @@ static["__CONFIGS"] = []
 for group in static_groups:
     static["__" + group] = []
 
+generic_exec_suffix = 0
 for conf in static_section:
-    # take the configuration name and add it to the data
-    static["__CONFIGS"].append(conf["__exec_suffix"])
+    # check for __exec_suffix keyword
+    if "__exec_suffix" in conf:
+        # take the configuration name and add it to the data
+        static["__CONFIGS"].append(conf["__exec_suffix"])
 
-    # check for key/value pairs in subgroups and add lists to the dictionary
-    for group in static_groups:
-        for key in conf["__STATIC"][group]:
-            if key not in static["__" + group]:
-                static["__" + group].append(key)
+        # check for key/value pairs in subgroups and add lists to the dictionary
+        for group in static_groups:
+            for key in conf["__STATIC"][group]:
+                if key not in static["__" + group]:
+                    static["__" + group].append(key)
 
-    # copy the entire data
-    static[conf["__exec_suffix"]] = conf["__STATIC"]
+        # copy the entire data
+        static[conf["__exec_suffix"]] = conf["__STATIC"]
+    else:
+        # append an integer
+        # TODO this assumes either no suffixes are given or all. otherwise (rare) there could be name clashes
+        static["__CONFIGS"].append(str(generic_exec_suffix))
+        generic_exec_suffix += 1
 
 # print to CMake
 printForCMake(static)
