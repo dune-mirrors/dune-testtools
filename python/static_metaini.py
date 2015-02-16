c93a58455c0b7d7feab4be6e710b79419d2baac5
Timo Koch <koch_timo@hotmail.com>
2015-02-16 19:09:12 +0100

    Only pass a static dictionary to CMake if there is more than one target to be build.
    CMake can then check for empty output. If only one target the name will be the basename.

--- python/static_metaini.py
+++ python/static_metaini.py
@@ -18,37 +18,40 @@ for conf in static_section:
             if (type(conf["__STATIC"][key]) is dict) and (key not in static_groups):
                 static_groups.append(key)
     else:
+        # debug output
         sys.stderr.write("\n -- Static variant generation failed because no __STATIC section could be found in the meta-inifile!\n")
-        exit(0)
 
 # construct a dictionary from the static information. This can be passed to CMake
-# The special key __CONFIGS holds a list of configuration names
 static = {}
-static["__CONFIGS"] = []
-# introduce a special key for all subgroups
-for group in static_groups:
-    static["__" + group] = []
+# only pass a dictionary to CMake if there is more than one target to be build
+if(len(static_section) > 1):
+    # The special key __CONFIGS holds a list of configuration names
+    static["__CONFIGS"] = []
+    # introduce a special key for all subgroups
+    for group in static_groups:
+        static["__" + group] = []
 
-generic_exec_suffix = 0
-for conf in static_section:
-    # check for __exec_suffix keyword
-    if "__exec_suffix" in conf:
-        # take the configuration name and add it to the data
-        static["__CONFIGS"].append(conf["__exec_suffix"])
+    generic_exec_suffix = 0
 
-        # check for key/value pairs in subgroups and add lists to the dictionary
-        for group in static_groups:
-            for key in conf["__STATIC"][group]:
-                if key not in static["__" + group]:
-                    static["__" + group].append(key)
+    for conf in static_section:
+        # check for __exec_suffix keyword
+        if "__exec_suffix" in conf:
+            # take the configuration name and add it to the data
+            static["__CONFIGS"].append(conf["__exec_suffix"])
 
-        # copy the entire data
-        static[conf["__exec_suffix"]] = conf["__STATIC"]
-    else:
-        # append an integer
-        # TODO this assumes either no suffixes are given or all. otherwise (rare) there could be name clashes
-        static["__CONFIGS"].append(str(generic_exec_suffix))
-        generic_exec_suffix += 1
+            # check for key/value pairs in subgroups and add lists to the dictionary
+            for group in static_groups:
+                for key in conf["__STATIC"][group]:
+                    if key not in static["__" + group]:
+                        static["__" + group].append(key)
 
-# print to CMake
-printForCMake(static)
+            # copy the entire data
+            static[conf["__exec_suffix"]] = conf["__STATIC"]
+        else:
+            # append an integer
+            # TODO this assumes either no suffixes are given or all. otherwise (rare) there could be name clashes
+            static["__CONFIGS"].append(str(generic_exec_suffix))
+            generic_exec_suffix += 1
+
+    # print to CMake
+    printForCMake(static)
