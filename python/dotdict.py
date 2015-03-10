3dfe6a0e33c4065bcb3c469aa0ac7ab00c7ecf5e
Dominic Kempf <dominic.r.kempf@gmail.com>
2015-03-10 13:24:27 +0100

    Add a prototype implementation of a DotDict

---
+++ python/dotdict.py
@@ -0,0 +1,26 @@
+""" A module defining a dictionary, that allows ot access nested structures by having dots in keys
+
+d["a"]["b"] ==  d["a.b"]
+"""
+
+from escapes import exists_unescaped, escaped_split
+
+class DotDict(dict):
+    def __init__(self, *args, **kwargs):
+        dict.__init__(self, *args, **kwargs)
+
+    def __getitem__(self, key):
+        if exists_unescaped(key, "."):
+            group, key = escaped_split(key, ".", maxsplit=1)
+            return dict.__getitem__(self, group)[key]
+        else:
+            return dict.__getitem__(self, key)
+
+    def __setitem__(self, key, value):
+        if exists_unescaped(key, "."):
+            group, key = escaped_split(key, ".", maxsplit=1)
+            if not group in self:
+                dict.__setitem__(self, group, DotDict())
+            dict.__getitem__(self, group).__setitem__(key, value)
+        else:
+            dict.__setitem__(self, key, value)
