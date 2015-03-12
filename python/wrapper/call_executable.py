cbd089b90f042742d25eea716a6abfab7c793492
Timo Koch <koch_timo@hotmail.com>
2015-03-12 15:10:43 +0100

    fixed typo

--- python/wrapper/call_executable.py
+++ python/wrapper/call_executable.py
@@ -10,7 +10,7 @@ def call(executable, inifile=None):
         iniargument = inifile
         iniinfo = parse_ini_file(inifile)
         if "__inifile_optionkey" in iniinfo:
-            optionkey = " " + iniifile["__inifile_optionkey"] + " " + iniargument
+            optionkey = " " + iniinfo["__inifile_optionkey"] + " " + iniargument
 
     return os.system("./" + executable + " " + iniargument)
 
