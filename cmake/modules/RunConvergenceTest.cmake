5951394e01fa723d2e028e61d82bef513f3c73f3
Timo Koch <koch_timo@hotmail.com>
2015-03-18 19:09:10 +0100

    clean code from debug output

--- cmake/modules/RunConvergenceTest.cmake
+++ cmake/modules/RunConvergenceTest.cmake
@@ -9,13 +9,9 @@ string(REPLACE "+" ";" inis ${CONVERGENCE_TEST_INIS})
 foreach(ini ${inis})
   # Execute the test-executable
   set(ENV{PYTHONPATH} "${DUNE_TESTTOOLS_PATH}/python:$ENV{PYTHONPATH}")
-  message(STATUS "$ENV{PYTHONPATH}")
-  message(STATUS "PYTHON_EXECUTABLE: ${PYTHON_EXECUTABLE}")
-  message(STATUS "COMMAND: ${PYTHON_EXECUTABLE} ${CONVERGENCE_TEST_SCRIPT} --exec ${CONVERGENCE_TEST_TARGET} --ini ${ini}")
   execute_process(COMMAND ${PYTHON_EXECUTABLE} ${CONVERGENCE_TEST_SCRIPT} --exec ${CONVERGENCE_TEST_TARGET} --ini ${ini}
                   RESULT_VARIABLE result ERROR_VARIABLE output OUTPUT_VARIABLE output)
 
-  message(STATUS "${output}")
   # Check its return status
   if(result)
     message(FATAL_ERROR "Test run with ${CONVERGENCE_TEST_TARGET} and inifile ${ini} failed")
