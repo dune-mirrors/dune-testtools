f8df584db3b5bc743e786267715398aefe296f5d
Timo Koch <koch_timo@hotmail.com>
2015-03-19 12:05:47 +0100

    [fix] Fix the correct function name in the fuzzy compare test

--- python/tests/test_fuzzycomparevtk.py
+++ python/tests/test_fuzzycomparevtk.py
@@ -1,12 +1,12 @@
 """ A test for the vtu fuzzy compare"""
 
-from fuzzy_compare_vtk import *
+from fuzzy_compare_vtk import compare_vtk
 
 def test_vtu1_vtu2():
-	assert(is_fuzzy_equal_vtk("./tests/vtu1.vtu", "./tests/vtu2.vtu") == 0)
-	assert(is_fuzzy_equal_vtk("./tests/vtu1.vtu", "./tests/vtu21.vtu") == 1)
+	assert(compare_vtk("./tests/vtu1.vtu", "./tests/vtu2.vtu") == 0)
+	assert(compare_vtk("./tests/vtu1.vtu", "./tests/vtu21.vtu") == 1)
 
 #TODO fails because different grid ordering is not implemented yet
 def test_vtu1_vtu3():
 	#assert(is_fuzzy_equal_vtk("./tests/vtu1.vtu", "./tests/vtu3.vtu") == 0)	
-	assert(is_fuzzy_equal_vtk("./tests/vtu1.vtu", "./tests/vtu31.vtu") == 1)
+	assert(compare_vtk("./tests/vtu1.vtu", "./tests/vtu31.vtu") == 1)
