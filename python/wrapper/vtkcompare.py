fb0abb95745607ba10a4a623d7a793744e8a9079
Dominic Kempf <dominic.r.kempf@gmail.com>
2015-03-25 17:37:17 +0100

    Remove debug statements!

--- python/wrapper/vtkcompare.py
+++ python/wrapper/vtkcompare.py
@@ -15,8 +15,6 @@ if __name__ == "__main__":
     if ret is 0:
         # Parse the inifile to learn about where the vtk file and its reference solution are located.
         ini = parse_ini_file(args["ini"])
-        sys.stderr.write(ini["vtk.name"] + ".vtu")
-        sys.stderr.write(args["source"] + ini["vtk.reference"] + ".vtu")
         ret = compare_vtk(ini["vtk.name"] + ".vtu", args["source"] + "/" + ini["vtk.reference"] + ".vtu")
 
     sys.exit(ret)
