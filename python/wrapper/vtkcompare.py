8dd73bd140077e7ead4e5f83bf7b5c305652711a
Dominic Kempf <dominic.r.kempf@gmail.com>
2015-04-10 10:56:05 +0200

    Do a bit more error checking
    Use a try/except statement and write something meaningful to the test
    log if we exited on a key error.

--- python/wrapper/vtkcompare.py
+++ python/wrapper/vtkcompare.py
@@ -15,7 +15,13 @@ if __name__ == "__main__":
     if ret is 0:
         # Parse the inifile to learn about where the vtk file and its reference solution are located.
         ini = parse_ini_file(args["ini"])
-        ext = ini.get("vtk.extension", "vtu")
-        ret = compare_vtk(ini["vtk.name"] + "." + ext, args["source"] + "/" + ini["vtk.reference"] + "." + ext)
+        try:
+            solution = ini["vtk.name"]
+            ext = ini.get("vtk.extension", "vtu")
+            reference = ini["vtk.reference"]
+            ret = compare_vtk(solution + "." + ext, args["source"] + "/" + reference + "." + ext)
+        except KeyError:
+            sys.stdout.write("The test wrapper vtkcompare assumes keys vtk.name and vtk.reference to be existent in the inifile")
+            ret = 1
 
     sys.exit(ret)
