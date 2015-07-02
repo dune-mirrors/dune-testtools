0b5c0b4a5bc2aca9a64c3280538447de2b19a2c7
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2015-07-02 15:22:56 +0200

    Remove debug output in cmake macro

--- cmake/modules/DuneTesttoolsMacros.cmake
+++ cmake/modules/DuneTesttoolsMacros.cmake
@@ -102,7 +102,6 @@ endif()
 # Check for the existence of the python-pyparsing package
 if(NOT CMAKE_CROSSCOMPILING)
   execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import pyparsing" RESULT_VARIABLE PYPARSING_RETURN)
-  message("PYPARSING_RETURN: ${PYPARSING_RETURN}")
   if(NOT PYPARSING_RETURN STREQUAL "0")
     message(FATAL_ERROR "dune-testtools requires the package python-pyparsing to be present on the system")
   endif()
