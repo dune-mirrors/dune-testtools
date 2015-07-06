8974e588c976b16def223cd2591ac3eeb75a5203
Dominic Kempf <dominic.r.kempf@gmail.com>
2015-07-06 17:18:00 +0200

    Implement a label command.
    It takes two arguments: The first is the label
    to apply, if the conditional evaluates to true.
    
    The (optional) second one defines the name of the
    label group and defaults to PRIORITY.
    
    The resulting labels are put into the __STATIC.LABELS
    section of the output, where the Python-CMake-interface
    will pick them up in the future.
    
    Check the example cond2.mini for a comprehensive test.

--- python/conditionals.py
+++ python/conditionals.py
@@ -5,3 +5,8 @@ from .command import meta_ini_command, CommandType
 @meta_ini_command(name="exclude", returnConfigs=True)
 def _exclude(configs=None, key=None):
     return [c for c in configs if not eval(c[key])]
+
+@meta_ini_command(name="label", argc=2, argdefaults=[None, "PRIORITY"], returnValue=False)
+def _label(config=None, value=None, args=None):
+    if eval(value):
+        config["__STATIC.LABELS." + args[1]] = args[0]
