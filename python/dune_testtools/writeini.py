5800e7c80cacf182cfaf9230d346a50b7c9f4733
Dominic Kempf <dominic.r.kempf@gmail.com>
2015-07-08 18:30:15 +0200

    Allow writing ini files to arbitrary streams instead of files
    Actually, by stream I am referring to the C++ concept of a stream.
    I am not sure they call it like that in python. Anythin with a write
    method works.

--- python/dune_testtools/writeini.py
+++ python/dune_testtools/writeini.py
@@ -1,5 +1,30 @@
 """ A module for writing ini files to disc """
 
+def write_to_stream(d, stream, assignment="="):
+    def traverse_dict(stream, d, prefix):
+        # first traverse all non-group values (they would otherwise be considered part of a group)
+        for key, value in sorted(dict.items(d)):
+            if not isinstance(value, dict):
+                stream.write("{} {} {}\n".format(key, assignment, value))
+
+        # now go into subgroups
+        for key, value in sorted(dict.items(d)):
+            if isinstance(value, dict):
+                pre = prefix + [key]
+
+                def groupname(prefixlist):
+                    prefix = ""
+                    for p in prefixlist:
+                        if prefix is not "":
+                            prefix = prefix + "."
+                        prefix = prefix + p
+                    return prefix
+
+                stream.write("\n[{}]\n".format(groupname(pre)))
+                traverse_dict(stream, value, pre)
+
+    prefix = []
+    traverse_dict(stream, d, prefix)
 
 def write_dict_to_ini(d, filename, assignment="="):
     """ Write a (nested) dictionary to a file following the ini file syntax:
@@ -16,28 +41,5 @@ def write_dict_to_ini(d, filename, assignment="="):
     assignment : string
         the character(s) which should be used for assigning values to keys
     """
-    with open(filename, 'w') as f:
-        def traverse_dict(file, d, prefix):
-            # first traverse all non-group values (they would otherwise be considered part of a group)
-            for key, value in sorted(dict.items(d)):
-                if not isinstance(value, dict):
-                    file.write("{} {} {}\n".format(key, assignment, value))
-
-            # now go into subgroups
-            for key, value in sorted(dict.items(d)):
-                if isinstance(value, dict):
-                    pre = prefix + [key]
-
-                    def groupname(prefixlist):
-                        prefix = ""
-                        for p in prefixlist:
-                            if prefix is not "":
-                                prefix = prefix + "."
-                            prefix = prefix + p
-                        return prefix
-
-                    file.write("\n[{}]\n".format(groupname(pre)))
-                    traverse_dict(file, value, pre)
-
-        prefix = []
-        traverse_dict(f, d, prefix)
+    f = open(filename, 'w')
+    write_to_stream(d, f, assignment=assignment)
