598344d9143ca93597268292d82a5f44a83f1d3c
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2015-07-17 21:19:28 +0200

    Make pre and post flags optional. Throw better error message if mpi-exec or mpi-numprocflags were not given

--- python/dune_testtools/wrapper/call_parallel.py
+++ python/dune_testtools/wrapper/call_parallel.py
@@ -32,8 +32,15 @@ def call(executable, mpi_exec, mpi_numprocflag, mpi_preflags, mpi_postflags, ini
 if __name__ == "__main__":
     # Parse the given arguments
     args = get_args()
-    if not all([args["mpi_exec"], args["mpi_numprocflag"], args["mpi_preflags"], args["mpi_postflags"]]):
-        sys.stderr.write("No suitable MPI version was found on the system!")
+    if not args["mpi_exec"]:
+        sys.stderr.write("call_parallel.py: error: Mpi executable not given.\n" +
+                          "usage: call_parallel.py [-h] -e EXEC -i INI --mpi-exec MPI_EXEC \n" +
+                          "                        --mpi-numprocflag MPI_NUMPROCFLAG [-s SOURCE]\n")
+        sys.exit(1)
+    if not args["mpi_numprocflag"]:
+        sys.stderr.write("call_parallel.py: error: Mpi number of processes flag not given.\n" +
+                         "usage: call_parallel.py [-h] -e EXEC -i INI --mpi-exec MPI_EXEC \n" +
+                         "                         --mpi-numprocflag MPI_NUMPROCFLAG [-s SOURCE]\n")
         sys.exit(1)
     # check if flags are  provided
     if args["mpi_preflags"] == ['']:
