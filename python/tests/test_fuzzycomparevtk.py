8039189a30ab5a1ce1205f3e40b2e3c447cbcd6d
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2015-08-07 15:28:15 +0200

    [test,fuzzy] Add a test testing the zeroThreshold

--- python/tests/test_fuzzycomparevtk.py
+++ python/tests/test_fuzzycomparevtk.py
@@ -3,17 +3,29 @@ from __future__ import absolute_import
 from dune_testtools.wrapper.fuzzy_compare_vtk import compare_vtk
 
 
-def test_0(dir):
-    assert(compare_vtk(dir + "vtu1.vtu", dir + "vtu2.vtu") == 0)
+def test_failing_comparison(dir):
+    assert(compare_vtk(dir + "vtu.vtu", dir + "vtu1.vtu") == 1)
 
 
-def test_1(dir):
-    assert(compare_vtk(dir + "vtu1.vtu", dir + "vtu21.vtu") == 0)
+def test_different_parameter_order(dir):
+    assert(compare_vtk(dir + "vtu.vtu", dir + "vtu2.vtu") == 0)
 
 
-def test_2(dir):
-    assert(compare_vtk(dir + "vtu1.vtu", dir + "vtu3.vtu") == 0)
+def test_different_grid_order(dir):
+    assert(compare_vtk(dir + "vtu.vtu", dir + "vtu3.vtu") == 0)
 
 
-def test_3(dir):
-    assert(compare_vtk(dir + "vtu1.vtu", dir + "vtu31.vtu") == 1)
+def test_neglegible_difference(dir):
+    assert(compare_vtk(dir + "vtu.vtu", dir + "vtu4.vtu") == 0)
+
+
+def test_close_to_zero_difference(dir):
+    assert(compare_vtk(dir + "vtu5.vtu", dir + "vtu6.vtu") == 1)
+
+
+def test_close_to_zero_difference_zerothreshold(dir):
+    assert(compare_vtk(dir + "vtu5.vtu", dir + "vtu6.vtu", zeroValueThreshold={"small": 1e-20}) == 1)
+
+
+def test_close_to_zero_difference_zerothreshold2(dir):
+    assert(compare_vtk(dir + "vtu5.vtu", dir + "vtu6.vtu", zeroValueThreshold={"small": 1e-19}) == 0)
