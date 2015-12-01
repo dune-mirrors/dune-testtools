d993dd3e165a2a1668493a84ffe09b0044abb89c
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2015-12-01 16:59:55 +0100

    [wrapper] Make vtk compare multiple vtks
    Multiple vkts can be specified by comma separated lists
    in the input file keys wrapper.vtkcompare.name/extension/reference.
    
    Options can be passed for each test under
    wrapper.vtkcompare.<vtkname>.absolute/relative/zeroThreshold
    If there is just one vtk file comparison the vtkname subgroup is omitted.

--- python/wrapper/vtkcompare.py
+++ python/wrapper/vtkcompare.py
@@ -18,15 +18,36 @@ ret = call(args["exec"], args["ini"])
 
 # do the vtk comparison if execution was succesful
 if ret is 0:
-    # Parse the inifile to learn about where the vtk file and its reference solution are located.
+    # Parse the inifile to learn about where the vtk files and its reference solutions are located.
     ini = parse_ini_file(args["ini"])
     try:
-        solution = ini["vtk.name"]
-        ext = ini.get("vtk.extension", "vtu")
-        reference = ini["vtk.reference"]
-        ret = compare_vtk(solution + "." + ext, args["source"] + "/" + reference + "." + ext)
+        # get reference solutions
+        names = ini["wrapper.vtkcompare.name"].split(' ')
+        exts = ini.get("wrapper.vtkcompare.extension", "vtu " * len(names)).split(' ')
+        references = ini["wrapper.vtkcompare.reference"].split(' ')
     except KeyError:
-        sys.stdout.write("The test wrapper vtkcompare assumes keys vtk.name and vtk.reference to be existent in the inifile")
-        ret = 1
+        sys.stdout.write("The test wrapper vtkcompare assumes keys wrapper.vtkcompare.name \
+                          and wrapper.vtkcompare.reference to be existent in the inifile")
 
-sys.exit(ret)
+    # loop over all vtk comparisons
+    for n, e, r in zip(names, exts, references):
+        # if we have multiple vtks search in the subgroup prefixed with the vtk-name for options
+        prefix = "" if len(names) == 1 else n + "."
+
+        # check for specific options for this comparison
+        relative = float(ini.get("wrapper.vtkcompare." + prefix + "relative", 1e-2))
+        absolute = float(ini.get("wrapper.vtkcompare." + prefix + "absolute", 1.5e-7))
+        zeroThreshold = ini.get("wrapper.vtkcompare." + prefix + "zeroThreshold", {})
+
+        ret = compare_vtk(vtk1=n + "." + e,
+                          vtk2=args["source"] + "/" + r + "." + e,
+                          absolute=absolute,
+                          relative=relative,
+                          zeroValueThreshold=zeroThreshold,
+                          verbose=True)
+
+        # early exit if one vtk comparison fails
+        if ret is not 0:
+            sys.exit(ret)
+
+sys.exit(0)
