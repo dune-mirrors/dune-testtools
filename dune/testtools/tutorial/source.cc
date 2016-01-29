b9299af662c2c3e73a534efa64c7e1fba7c6f35d
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2016-01-29 14:18:30 +0100

    [tutorial][fix] Implement error handling. This fixes #103.
    (cherry picked from commit 724cf02cc02b63e001cd06875885c312d17b5c88)

--- dune/testtools/tutorial/source.cc
+++ dune/testtools/tutorial/source.cc
@@ -7,6 +7,7 @@
 #include <sstream>
 #include <string>
 #include <dune/common/parallel/mpihelper.hh>
+#include <dune/common/exceptions.hh>
 #include <dune/common/parametertree.hh>
 #include <dune/common/parametertreeparser.hh>
 #include <dune/grid/utility/structuredgridfactory.hh>
@@ -14,10 +15,14 @@
 #include <dune/grid/yaspgrid.hh>
 #include <dune/grid/uggrid.hh>
 
-int main(int argc, char** argv)
+int main(int argc, char** argv) try
 {
   // maybe initialize mpi
   Dune::MPIHelper::instance(argc, argv);
+
+  // check if a single argument was supplied
+  if (argc != 2)
+    DUNE_THROW(Dune::InvalidStateException, "Please supply an ini file. Usage: ./" << argv[0] << " <inifile>");
 
   // load the parameter file
   Dune::ParameterTree params;
@@ -40,4 +45,11 @@ int main(int argc, char** argv)
   std::stringstream outputName;
   outputName << argv[0] << "_" << params.get<std::string>("level");
   vtkwriter.write(outputName.str());
+
+  return 0;
+}
+// Error handler /////////////////
+catch (Dune::Exception e) {
+    std::cerr << e << std::endl;
+    return 1;
 }
